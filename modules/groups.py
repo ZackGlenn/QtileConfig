b8cc87ff7b53dc39255b8adf5bd97c2ffebbc2f3
Zack Glenn <ZackGlenn05@gmail.com>
2023-05-17 11:51:12 -0600

    improve group handling workflow

--- modules/groups.py
+++ modules/groups.py
@@ -28,3 +28,17 @@ for i in groups:
         Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
             desc="move focused window to group {}".format(i.name)),
     ])
+
+keys.extend([
+    Key([mod], "y", lazy.group["1"].toscreen(), desc = "Switch to group 1"),
+    Key([mod], "u", lazy.group["2"].toscreen(), desc = "Switch to group 2"),
+    Key([mod], "i", lazy.group["3"].toscreen(), desc = "Switch to group 3"),
+    Key([mod], "o", lazy.group["4"].toscreen(), desc = "Switch to group 4"),
+    Key([mod], "p", lazy.group["5"].toscreen(), desc = "Switch to group 5"),
+
+    Key([mod, "shift"], "y", lazy.window.togroup("1"), desc = "Move focused window to group 1"),
+    Key([mod, "shift"], "u", lazy.window.togroup("2"), desc = "Move focused window to group 2"),
+    Key([mod, "shift"], "i", lazy.window.togroup("3"), desc = "Move focused window to group 3"),
+    Key([mod, "shift"], "o", lazy.window.togroup("4"), desc = "Move focused window to group 4"),
+    Key([mod, "shift"], "p", lazy.window.togroup("5"), desc = "Move focused window to group 5"),
+])
