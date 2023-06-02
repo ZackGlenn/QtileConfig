4e4a338e846b3a0e32f4b96b4cf43b32f030878d
Zack Glenn <ZackGlenn05@gmail.com>
2023-06-01 22:34:01 -0600

    move scripts; add autostart after

--- modules/hooks.py
+++ modules/hooks.py
@@ -3,17 +3,18 @@ from modules.screens import get_num_monitors
 import subprocess
 import os
 
-qtile_path = os.path.expanduser('~/.config/qtile/')
+qtile_scripts_path = os.path.expanduser('~/.config/qtile/scripts/')
 
 @hook.subscribe.startup_once
 def autostart():
-    subprocess.call([qtile_path + 'autostart_once.sh'])
+    subprocess.call([qtile_scripts_path + 'autostart_once.sh'])
 
 @hook.subscribe.startup
 def always_autostart():
-    subprocess.call([qtile_path + 'autostart_always.sh'])
+    subprocess.call([qtile_scripts_path + 'autostart_always.sh'])
 
 @hook.subscribe.startup_complete
 def assign_groups_to_screens():
     if get_num_monitors() > 1:
         qtile.groups_map["2"].cmd_toscreen(1, toggle=False)
+    subprocess.call([qtile_scripts_path + 'autostart_after.sh'])
