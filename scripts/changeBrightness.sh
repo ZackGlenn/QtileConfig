3645bd6bf1650a9bbc1b681c6d2e0b868f7599e8
Zack Glenn <ZackGlenn05@gmail.com>
2024-03-01 15:46:02 -0700

    avoid a magic number for max brightness

--- scripts/changeBrightness.sh
+++ scripts/changeBrightness.sh
@@ -4,11 +4,14 @@
 # Arbitrary but unique message tag
 msgTag="mybrightness"
 
+# Maximum panel brightness, for computing percentage
+maxBrightness="4285"
+
 # Change the brightness using light
 brightnessctl set "$@" >/dev/null
 
 # Query light for the current brightness
-brightness="$((100 * $(brightnessctl get) / 4285))"
+brightness="$((100 * $(brightnessctl get) / maxBrightness))"
 
 # Show the brightness notification
 dunstify -a "changeBrightness" -u low -h string:x-dunst-stack-tag:$msgTag \
