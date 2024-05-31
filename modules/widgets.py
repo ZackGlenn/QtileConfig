8c8b5c6180f74d20c0d8d0b3b6cad417016ab3d7
Zack Glenn <ZackGlenn05@gmail.com>
2024-05-31 11:57:03 -0600

    use default volume widget to fix issue with get_volume()

--- modules/widgets.py
+++ modules/widgets.py
@@ -19,40 +19,12 @@ widget_defaults = dict(
     padding=3,
 )
 extension_defaults = widget_defaults.copy()
-class MyVolume(widget.Volume):
-    def _configure(self, qtile, bar):
-        widget.Volume._configure(self, qtile, bar)
-        self.volume = self.get_volume()
-        if self.volume <= 0:
-            self.text = '󰆪'
-        elif self.volume <= 15:
-            self.text = ''
-        elif self.volume < 50:
-            self.text = ''
-        else:
-            self.text = ''
-        # drawing here crashes Wayland
-
-    def _update_drawer(self, wob=False):
-        if self.volume <= 0:
-            self.text = '󰆪'
-        elif self.volume <= 15:
-            self.text = ''
-        elif self.volume < 50:
-            self.text = ''
-        else:
-            self.text = ''
-        self.draw()
-
-        if wob:
-            with open(self.wob, 'a') as f:
-                f.write(str(self.volume) + "\n")
-
-
-volume = MyVolume(
-    fontsize=18,
+volume = widget.Volume(
+    fontsize=24,
     font='Font Awesome 5 Free',
     foreground=colors[4],
     background='#2f343f',
-    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn("pavucontrol")}
+    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn("pavucontrol")},
+    emoji = True,
+    emoji_list = ['󰆪', '', '', '']
 )
