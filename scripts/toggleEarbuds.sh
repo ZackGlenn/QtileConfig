fa84ae1676f3dca5c053c7b8de88c8d14ce57d18
Zack Glenn <ZackGlenn05@gmail.com>
2024-07-21 09:35:34 -0600

    update earbud toggling script for new earbuds

--- scripts/toggleEarbuds.sh
+++ scripts/toggleEarbuds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Toggle connection to galaxy earbuds and capture any output
-msg=$(bluetoggle -d 1 -a C4:5D:83:C5:9A:EF 2>&1)
+msg=$(bluetoggle -d 1 -a DC:69:E2:23:CA:6F 2>&1)
 
 # If there is output, there was an error which should be reported
 if not [ "$msg" = "" ]; then dunstify -a "toggleEarbuds" -u low "$msg"; fi
